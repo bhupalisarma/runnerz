110be39d6e51ea4bc19562483ebbf94d0c09bb2e
bhupalisarma <bhupali.sarma@ubona.com>
2024-07-11 15:34:17 +0530

    API working in Postman.

--- runnerz/src/main/java/com/example/runnerz/run/RunController.java
+++ runnerz/src/main/java/com/example/runnerz/run/RunController.java
@@ -1,13 +1,38 @@
 package com.example.runnerz.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/runs")
 public class RunController {
+    private RunRepository runRepository;
+    public RunController(RunRepository runRepository){
+        this.runRepository = runRepository;
+    }
 
+    @GetMapping("")
+    List<Run> findAll(){
+        return runRepository.findAll();
+    };
 
+    @GetMapping("/{id}")
+        Run findById(@PathVariable Integer id){
+        Optional<Run> run = runRepository.findById(id);
+        if(run.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return run.get();
+    }
 
-
-
+    //post
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@RequestBody Run run){
+        runRepository.create(run);
+    }
 }
--- runnerz/src/main/java/com/example/runnerz/run/RunRepository.java
+++ runnerz/src/main/java/com/example/runnerz/run/RunRepository.java
@@ -1,15 +1,30 @@
 package com.example.runnerz.run;
 import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
+@Repository
 public class RunRepository {
     private List<Run> runs = new ArrayList<>();
     List<Run> findAll(){
         return runs;
     }
+
+    Optional<Run> findById(Integer id){
+        return runs.stream()
+                .filter(run -> run.id().equals(id))
+                .findFirst();
+    }
+
+    void create(Run run){
+        runs.add(run);
+    }
+
     @PostConstruct
     private void init(){
         runs.add(new Run(
